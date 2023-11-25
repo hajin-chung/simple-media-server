ee5c23860d4c2e6f0a33012f5c802dcfc8d197db
hajin-chung <hajinchung1@gmail.com>
2023-11-25 21:29:46 +0900

    responsive design for mobile

--- static/text.js
+++ static/text.js
@@ -1,8 +1,10 @@
-const path = document.querySelector("#title").innerHTML;
-const content = document.querySelector("pre");
+(() => {
+  const path = document.location.pathname;
+  const content = document.querySelector("pre");
 
-fetch(`/data/${path}`)
-  .then(res => res.text())
-  .then(text => {
-    content.innerHTML = text;
-  });
+  fetch(`/data${path}`)
+    .then(res => res.text())
+    .then(text => {
+      content.innerHTML = text;
+    });
+})()
